27334bad4d0a21b1604d3464f78d349f86928634
DavidHuang <david.huang3214@gmail.com>
2023-09-19 21:26:42 -0700

    Add cors & other middleware

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ require('./passport/passport.js');
 
 const express = require('express');
 const path = require('path');
-const passport = require("passport");
+const passport = require('passport');
 const logger = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
 
 // routes
 const apiRouter = require('./routes/api');
@@ -22,13 +25,20 @@ async function main() {
 var app = express();
 
 // middleware
+let corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://bookshelf-client-eight.vercel.app/'],
+    optionsSuccessStatus: 200
+}
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
+app.options('*', cors(corsOptions));
 
 // routes
-app.use('/api', apiRouter);
+app.use('/api', cors(corsOptions), apiRouter);
 
 module.exports = app;
