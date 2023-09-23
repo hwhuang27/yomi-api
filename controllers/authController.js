432a63d0c65f41c7898dce4a7dc0ceb868105c6a
DavidHuang <david.huang3214@gmail.com>
2023-09-22 21:46:34 -0700

    Change jwt expiration

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ exports.login = async function (req, res, next){
                 }
                 // create token
                 const body = { _id: user._id, username: user.username};
-                const token = jwt.sign(body, process.env.SECRET_KEY, {expiresIn: '15s'});
+                const token = jwt.sign(body, process.env.SECRET_KEY, {expiresIn: '1d'});
                 
                 return res.status(200).json({ body, token });
             });
